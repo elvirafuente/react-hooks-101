65130229ff5b46075d4e3c811a3aab8c6a312cc7
Elvira Fuente Romero <elvira.fuente@ultimasoftware.com>
2019-07-26 14:01:13 +0200

    add function to manage both buttons

--- src/BasicCounter/index.js
+++ src/BasicCounter/index.js
@@ -1,18 +1,42 @@
 import React, {useState} from 'react';
 
 const BasicCounter = () => {
+    
+    
+    /////////////////////    // ////inline functions
+    // const [ counter, setCounter ] = useState(0);
+       
+    // return ( 
+    //     <div>
+    //         <h2 className="title">Welcome to the counter of life</h2>
+    //         <p>{counter}</p>
+    //         <button onClick={() => setCounter(counter + 1)}>Add (+)</button>
+    //         <button onClick={() => setCounter(counter - 1)}>Subtract (-)</button>
+    //     </div>
+    // );
 
     const [ counter, setCounter ] = useState(0);
+
+    const handleClick = (event) => {
+        const {id} = event.target;
+        if( id === "add"){
+            setCounter(counter + 1)
+        } else {
+            setCounter(counter - 1)
+
+        }
+    }
        
     return ( 
         <div>
             <h2 className="title">Welcome to the counter of life</h2>
             <p>{counter}</p>
-            <button onClick={() => setCounter(counter + 1)}>Add (+)</button>
-            <button onClick={() => setCounter(counter - 1)}>Subtract (-)</button>
-
+            <button onClick={handleClick} id="add">Add (+)</button>
+            <button onClick={handleClick} id="sub">Subtract (-)</button>
         </div>
     );
+
+
 }
 
 export default BasicCounter; 
